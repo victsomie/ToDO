e8792bf19fd6d726cb1489cbc275710ccfb18bea
victsomie <victsomie@yahoo.com>
2016-06-30 23:29:14 +0300

    removed fab

---
+++ app/src/main/java/com/example/victor/todo/ActivitySignUp.java
@@ -0,0 +1,13 @@
+package com.example.victor.todo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivitySignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity_sign_up);
+    }
+}
--- app/src/main/java/com/example/victor/todo/Constants.java
+++ app/src/main/java/com/example/victor/todo/Constants.java
@@ -4,4 +4,6 @@ package com.example.victor.todo;
  * Created by victor on 6/30/16.
  */
 public class Constants {
+    public static final String FIREBASE_URL = "https://victortodo.firebaseio.com"; // Replace with your own URL
 }
+
